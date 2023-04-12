fe7dff38ca1200001ca52117827fe8223d6e034e
chungjinlee <chungjinlee@chungjinleeui-MacBookPro.local>
2023-04-12 20:37:08 +0900

    feat : middleware

--- playground/express/app.js
+++ playground/express/app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.set('port', process.env.PORT ||3000);
 
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행하고 싶어요');
+    // next 사용 안하면 넘어가지 않는다.
+    next();
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -16,6 +22,13 @@ app.post('/', (req, res)=> {
 app.get('/about', (req, res) => {
     res.send('hello express');
 })
+
+app.get('/category/:name', (req, res) => {
+    res.send(`hello ${req.params.name}`);
+});
+app.get('/', (req, res) => {
+    res.send('hello express');
+});
 
 app.listen(3000, () => {
     console.log('익스프레스 서버 생성');
