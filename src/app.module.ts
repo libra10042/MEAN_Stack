d25738e97b196f6921427b8b65ae9d87d2576e0f
chungjinlee <chungjinlee@chungjinleeui-MacBookPro.local>
2023-04-23 11:39:43 +0900

    feat : global exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { CatsController } from './cats/controller/cats.controller';
 import { CatsService } from './cats/service/cats.service';
+import { HttpExceptionFilter } from './common/middleware/http-exception.filter';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [
+    CatsService,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/middleware/all-exception.filter';
+import { HttpExceptionFilter } from './common/middleware/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
