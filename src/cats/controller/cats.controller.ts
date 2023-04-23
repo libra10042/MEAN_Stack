3a2fd7614e2152d2c86ca580f1791451f3dec709
chungjinlee <chungjinlee@chungjinleeui-MacBookPro.local>
2023-04-23 11:29:45 +0900

    feat : single http exception filter

--- src/cats/controller/cats.controller.ts
+++ src/cats/controller/cats.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { CatsService } from '../service/cats.service';
 import { CreateCatDto } from '../dto/CreateCatDto';
 import { Cat } from '../interface/cat.interface';
+import { HttpExceptionFilter } from 'src/common/middleware/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UseFilters(new HttpExceptionFilter())
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
