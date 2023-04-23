80abb3200d03d695f5fbb995a8a51d7e4183f85d
chungjinlee <chungjinlee@chungjinleeui-MacBookPro.local>
2023-04-23 14:47:34 +0900

    fix : all-exception-filter

--- src/common/middleware/all-exception.filter.ts
+++ src/common/middleware/all-exception.filter.ts
@@ -9,26 +9,28 @@ import { HttpAdapterHost } from '@nestjs/core';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
-
-  catch(exception: unknown, host: ArgumentsHost): void {
-    // In certain situations `httpAdapter` might not be available in the
-    // constructor method, thus we should resolve it here.
-    const { httpAdapter } = this.httpAdapterHost;
-
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
 
-    const httpStatus =
+    const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const responseBody = {
-      statusCode: httpStatus,
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal server error';
+
+    const errorResponse = {
+      statusCode: status,
       timestamp: new Date().toISOString(),
-      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      path: request.url, // URL 정보를 직접 request 객체에서 가져옵니다.
+      message,
     };
 
-    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+    response.status(status).json(errorResponse);
   }
 }
